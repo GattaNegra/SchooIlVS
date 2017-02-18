ec6decc5a2988f37e96c1aff80f2a1289e0ad258
gattanegra <gattanegrabg@gmail.com>
2017-02-18 13:04:54 +0200

    i05 loops

---
+++ VisualStudio/05-Loops/nummax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nummax
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int n= int.Parse(Console.ReadLine ());
+			var max = int.MinValue;
+			for (int i = 0; i < n; i++) 
+				
+				
+			{
+				int inputNumber=int.Parse( 	Console.ReadLine ());
+				if (inputNumber>max) 
+					
+				{
+					Console.WriteLine (inputNumber);
+
+				}
+			}
+
+		}
+	}
+}
